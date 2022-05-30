23655f50cc56964135f866e88ded1efe45460fd7
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2022-05-29 21:43:24 -0300

    Agrego el nombre del brunch a la MyNavBar dentro del .navbar-brand

--- src/components/MyNavBar.jsx
+++ src/components/MyNavBar.jsx
@@ -5,7 +5,7 @@ function MyNavBar() {
     return (
     <>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-          <a className="navbar-brand" href="#"><img src={logo4} alt="Logo Martín NFT" /></a>
+          <a className="navbar-brand" href="#"><img src={logo4} alt="Logo Martín NFT" />Martín NFT</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
           </button>
