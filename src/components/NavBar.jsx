56b0e6832e414027bc0ea95c214c98ecf3e7ffdf
Mart-Dan-Rossi <martinrossi1993@gmail.com>
2022-05-30 19:34:42 -0300

    Cambios menores

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo4 from '../img/Logo4.png'
+import 'NavBar.css'
 
 function NavBar() {
     return (
@@ -7,7 +8,7 @@ function NavBar() {
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
           <a className="navbar-brand" href="#"><img src={logo4} alt="Logo Martín NFT" />Martín NFT</a>
           <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
+            <span className="navbar-toggler-icon" />
           </button>
           <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
             <div className="navbar-nav">
